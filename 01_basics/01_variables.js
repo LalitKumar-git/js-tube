08f2041eae14f82519dbca1155ff2a947e4510bc
LalitKumar-git <lalitthakuro817@gmail.com>
2025-10-22 08:37:31 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let email = "luvthakur@8533gmail.com"
+var accountPassword ="1234"
+city = "Jaipur"
+/*
+prefer not to use.var bcoz of issue in block scope and functional scope
+*/
+// accountId = 23.  Not allowed here
+email = "lalitthakuro817@gmail.com"
+accountPassword = "123321"
+city = "Bengaluru"
+console.log(accountId);
+console.table([accountId,email,accountPassword,city])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict" ; // treat all JS code as newer version
+ // alert(3 + 3), we are using nodejs not browser
+// Data Type	Description	Example
+// PRIMITIVE DATA TYPES
+//. String	Sequence of characters (text)	"Lalit", 'Hello', `JS`
+//  Number	Numeric values (integer or float)	42, 3.14, -10
+//. Boolean	Logical value — true or false	true, false
+//  Undefined	A variable declared but not assigned	let x; // undefined not defined yet
+//. Null	Represents intentional absence of value	let data = null; which means standalone values
+//. Symbol	Unique and immutable identifier	let id = Symbol('123');
+//. BigInt	Large integers beyond Number limit	let big = 1234567890123456789012345n;
+// symbol -> Uniqueness
+
+
+// NON - PRIMITIVE DATATYPES
+// object 
+// a imp point 
+console.log(typeOf null);
+console.log(typeOf undefined);
